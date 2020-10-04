84b8696febb17b0caa85232da761d3866b8caca2
lewis.wilson <lewis.j.wilson@outlook.com>
2020-10-04 23:57:18 +0900

    Level 1 Playable

---
+++ Assets/Scripts/AddConstantVelocity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddConstantVelocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3Force;
+
+    //called exactly 100 times per second
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody>().velocity += v3Force; //+= means we will add the value to every frame
+    }
+}
---
+++ Assets/Scripts/AddPlayerVelocity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPlayerVelocity : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 v3Force;
+
+    [SerializeField]
+    KeyCode keyPositive;
+
+    [SerializeField]
+    KeyCode keyNegative;
+
+    //called exactly 100 times per second
+    void FixedUpdate()
+    {
+        if (Input.GetKey(keyPositive))
+            GetComponent<Rigidbody>().velocity += v3Force; //+= means we will add the value to every frame
+
+        if (Input.GetKey(keyNegative))
+            GetComponent<Rigidbody>().velocity -= v3Force; //-= means we will subtract the value to every frame
+
+
+    }
+}
